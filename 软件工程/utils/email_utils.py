c291fca84ef2eaff19482982724dc6068991529c
yangshize888 <149244621+yangshize888@users.noreply.github.com>
2025-06-08 23:34:09 +0800

    Update email_utils.py

--- 软件工程/utils/email_utils.py
+++ 软件工程/utils/email_utils.py
@@ -5,7 +5,7 @@ def send_email(to_email, code):
     smtp_server = "smtp.qq.com"
     smtp_port = 587  # TLS
     sender_email = "1327470638@qq.com"
-    sender_auth_code = "miwjwkcdcinpicih"
+    sender_auth_code = "保密"
 
     subject = "验证码"
     body = f"【扇贝单词】您的验证码是：{code}。\n如非本人操作，请忽略此邮件。"
